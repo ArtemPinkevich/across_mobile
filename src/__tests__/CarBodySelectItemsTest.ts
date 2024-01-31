1aaf04f0d5be168d179e12e79876feb52d2a5dc5
PinkevichAV <PinkevichAV@elcomplus.ru>
2024-01-31 21:54:23 +0700

    Добавлен тест на мапер LoadingType enum

--- src/__tests__/CarBodySelectItemsTest.ts
+++ src/__tests__/CarBodySelectItemsTest.ts
@@ -1,7 +1,7 @@
 import { CarBodyType } from '../api/truck/CarBodyType';
 import { CARBODY_DISPLAY_NAME_MAP } from '../components/common/selectList/CarBodyToDisplayNameMap';
 
-it(`all CarBodyType exist in CARBODY_SELECT_ITEMS`, () => {
+it(`all CarBodyType exist in CARBODY_DISPLAY_NAME_MAP`, () => {
   const carBodiesArrayOfEnum = Object.values(CarBodyType).filter(o => !isNaN(Number(o)));
   const indexOfNotExistedCarBodyInTheMap = carBodiesArrayOfEnum.findIndex(o => !CARBODY_DISPLAY_NAME_MAP.has(o));
   expect(indexOfNotExistedCarBodyInTheMap).toBe(-1);
---
+++ src/__tests__/LoadingTypeToDisplayNameMapTest.ts
@@ -0,0 +1,8 @@
+import { LoadingType } from "../api/truck/LoadingType";
+import { LOADING_TYPE_DISPLAY_NAME_MAP } from "../components/common/selectList/LoadingTypeToDisplayNameMap";
+
+it(`all LoadingType exist in LOADING_TYPE_DISPLAY_NAME_MAP`, () => {
+  const loadingTypesArrayOfEnum = Object.values(LoadingType).filter(o => !isNaN(Number(o)));
+  const indexOfNotExistedLoadingTypeInTheMap = loadingTypesArrayOfEnum.findIndex(o => !LOADING_TYPE_DISPLAY_NAME_MAP.has(o));
+  expect(indexOfNotExistedLoadingTypeInTheMap).toBe(-1);
+});
