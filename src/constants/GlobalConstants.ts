33184171f510e7558d659fa53a9b559a217cde66
Nikita Chzhen <nch2591@gmail.com>
2024-09-23 04:10:48 +0700

    added initial map

--- src/constants/GlobalConstants.ts
+++ src/constants/GlobalConstants.ts
@@ -1,2 +1,4 @@
 export const SERVER_ADDRESS = "http://localhost:5000/api";
 export const GEO_SERVER_ADDRESS = "http://localhost:5004/api";
+//export const SERVER_ADDRESS = "http://192.168.1.7:5000/api";
+//export const GEO_SERVER_ADDRESS = "http://192.168.1.7:5004/api";
