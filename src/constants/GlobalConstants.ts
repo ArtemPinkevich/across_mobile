f061f9a78ee87e09477c1ac9a4c90d60c75dcbc9
PinkevichAV <PinkevichAV@elcomplus.ru>
2024-10-29 23:15:39 +0700

    Добавлен адрес домашнего сервера в GlobalConstants

--- src/constants/GlobalConstants.ts
+++ src/constants/GlobalConstants.ts
@@ -1,8 +1,14 @@
-export const SERVER_ADDRESS = "http://localhost:5000/api";
-export const GEO_SERVER_ADDRESS = "http://localhost:5004/api";
-export const OSRM_SERVER_ADDRESS = "http://127.0.0.1:5006/route/v1";
+// export const SERVER_ADDRESS = "http://localhost:5000/api";
+// export const GEO_SERVER_ADDRESS = "http://localhost:5004/api";
+//export const OSRM_SERVER_ADDRESS = "http://127.0.0.1:5006/route/v1";
+
 // export const SERVER_ADDRESS = "http://192.168.50.9:5000/api";
 // export const GEO_SERVER_ADDRESS = "http://192.168.50.9:5004/api";
 // export const OSRM_SERVER_ADDRESS = "http://192.168.50.9:5006/route/v1";
+
+// Джек
+export const OSRM_SERVER_ADDRESS = "http://147.30.192.92:5006/route/v1";
+export const SERVER_ADDRESS = "http://147.30.192.92:5000/api";
+export const GEO_SERVER_ADDRESS = "http://147.30.192.92:5004/api";
 
 export const LOGISTIC_COMMISSION = 0.07;
